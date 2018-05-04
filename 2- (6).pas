79d433e7354f446d43240e54af0be8528e6a0351
Maxminlevel <vvtoan35@gmail.com>
2018-05-04 16:36:18 +0700

    0.9

--- 2- (6).pas
+++ 2- (6).pas
@@ -4,6 +4,10 @@ function ucln(a,b:integer):integer;
  begin
  while  (a mod b <> 0) or (b mod a <> 0) do
  	if a>b then a:=a mod b else b:= b mod a;
+ {có thể dùng điều kiện while do sau để tìm ucln
+ while  a <> b do
+ 	if a>b then b:=a - b else a:= b - a;
+ }
  ucln:=a;
  end;
 begin
